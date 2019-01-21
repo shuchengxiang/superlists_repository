798436265fbdc6e3e7ea92a56e83eb1d68f1af40
shuchengxiang <993319134@qq.com>
2019-01-21 14:43:34 +0800

    commit before reconstruct in 5.4

--- functional_tests.py
+++ functional_tests.py
@@ -22,40 +22,46 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-    # 应用邀请她输入一个待办事项
+        # 应用邀请她输入一个待办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-    # 她在一个文本框中输入了“Buy peacock feathers”
-    # 伊迪斯的爱好是使用假蝇做饵钓鱼
+        # 她在一个文本框中输入了“Buy peacock feathers”
+        # 伊迪斯的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-    # 她按回车键后，页面更新了
-    # 待办事项表格中显示了“1：Buy peacock feathers”
+        # 她按回车键后，页面更新了
+        # 待办事项表格中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1：Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1：Buy peacock feathers', [row.text for row in rows])
 
-    # 页面中又显示了一个文本框，可以输入其他的待办事项
-    # 她输入了“Use peacock feathers to make fly”
-    # 伊迪斯做事很有条理
-        self.fail('Finish the test!')
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # 她输入了“Use peacock feathers to make fly”
+        # 伊迪斯做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn('1：Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2：Use peacock feathers to make fly',
+                      [row for row in rows])
+        # 伊迪斯想知道这个网站是否会记住她的清单
+        # 她看到网站为她生成了一个唯一的URL
+        # 而且页面中有一些文字解说这个功能
 
-    # 伊迪斯想知道这个网站是否会记住她的清单
-    # 她看到网站为她生成了一个唯一的URL
-    # 而且页面中有一些文字解说这个功能
+        self.fail('Finish the test!')
+        # 她访问那个URL，发现她的待办事项列表还在
 
-    # 她访问那个URL，发现她的待办事项列表还在
-
-    # 她很=满意，去睡觉了
+        # 她很=满意，去睡觉了
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
