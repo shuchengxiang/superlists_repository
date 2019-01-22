475d37e01f3a35bc45af138db195eeb62357d09e
shuchengxiang <993319134@qq.com>
2019-01-22 16:10:44 +0800

    commit before chaper 7.6

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -94,7 +94,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 弗朗西斯访问首页
         # 页面中看不到伊迪斯的清单
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
