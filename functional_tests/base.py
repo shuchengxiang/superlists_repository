d909509a57e50959e3cef35acf5d2e8deb6453a3
shuchengxiang <993319134@qq.com>
2019-03-07 10:15:55 +0800

    functional_tests use firefox

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -12,7 +12,7 @@ MAX_WAIT = 10
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
